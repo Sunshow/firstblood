684dc22aa9c664196afafcc8835b52bcdaefee30
Sunshow <sunshow@gmail.com>
2013-07-13 21:10:50 +0800

    先提交一个版本

--- src/web/multiconfirm/MulticonfirmConstant.java
+++
@@ -1,9 +0,0 @@
-package web.multiconfirm;
-
-public class MulticonfirmConstant {
-	public static String SPLIT_FLAG = ",";
-	public static String MULITCONFIRM_ACTIONNAME = "actionName";
-	public static String MULITCONFIRM_METHODNAME = "methodName";
-	public static final String MULTICONFIRM_SPECIAL_VALUE = "specialValue";
-	public static final String XML_SPLIT_FLAG = "_";
-}
