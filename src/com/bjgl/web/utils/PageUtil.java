076314a2be0abea20f38f209b89b9ed6832ef5b8
Sunshow <sunshow@gmail.com>
2013-07-17 09:51:36 +0800

    修正分页器代码

--- src/com/bjgl/web/utils/PageUtil.java
+++ src/com/bjgl/web/utils/PageUtil.java
@@ -15,60 +15,39 @@ public class PageUtil {
         if (pageBean.getPageCount() <= 0) {
             return "暂无分页！";
         }
-        StringBuffer sb = new StringBuffer();
-        String url = request.getRequestURI().toString();
+        StringBuffer buffer = new StringBuffer("<div class=\"pagination pagination-right\"><ul>");
+        //String url = request.getRequestURI().toString();
 
-        sb.append("<div class=\"pagination pagination-right\"><ul>\n" +
-                "    <li><a href=\"#\">Prev</a></li>\n" +
-                "    <li><a href=\"#\">1</a></li>\n" +
-                "    <li><a href=\"#\">2</a></li>\n" +
-                "    <li><a href=\"#\">3</a></li>\n" +
-                "    <li><a href=\"#\">4</a></li>\n" +
-                "    <li><a href=\"#\">5</a></li>\n" +
-                "    <li><a href=\"#\">Next</a></li>\n" +
-                "  </ul></div>");
+        int page = pageBean.getPage();
+        int total = pageBean.getCount();
 
-        return sb.toString();
-
-    }
-
-    public static String getSimplePageString(PageBean pageBean) {
-        if (pageBean.getPageCount() <= 0) {
-            return "暂无分页！";
+        // 是否显示上一页
+        if (page > 1) {
+            buffer.append("<li><a href=\"javascript://\" page=\"").append(page - 1).append("\">上一页</a></li>");
         }
-        StringBuffer sb = new StringBuffer();
-        //生成form
-        sb.append("共有").append("<span style='color:#EC8722;'>").append(pageBean.getPageCount()).append("</span>").append("页");
-        sb.append("&nbsp;<span style='color:#EC8722;'>").append(pageBean.getCount()).append("</span>").append("条记录");
-        sb.append("&nbsp;第");
-        sb.append("<select onchange='jumpPage(this.value)'>");
-        int showPageCount = 0;
-        if (pageBean.getPageCount() > 100) {
-            showPageCount = 100;
-        } else {
-            showPageCount = pageBean.getPageCount();
-        }
-        for (int i = 1; i <= showPageCount; i++) {
-            if (i == pageBean.getPage()) {
-                sb.append("<option value='").append(i).append("' selected>").append(i).append("</option>");
+
+        // 生成当前页前后4条，即最多显示9个页码
+
+        int start = (page - 4) <= 0 ? 1 : page - 4;
+        int end = (page + 4) > total ? total : page + 4;
+
+        for (int i = start; i <= end; i++) {
+            if (i == page) {
+                buffer.append("<li class=\"active\"><a href=\"javascript://\" page=\"").append(i).append("\">").append(i).append("</a></li>");
             } else {
-                sb.append("<option value='").append(i).append("'>").append(i).append("</option>");
+                buffer.append("<li><a href=\"javascript://\" page=\"").append(i).append("\">").append(i).append("</a></li>");
             }
         }
-        sb.append("</select>");
-        sb.append("页");
-        sb.append("&nbsp;<a style='color:#EC8722' href='javascript:jumpPage(1);'>首页</a>");
-        if (pageBean.getPage() > 1) {
-            sb.append("&nbsp;<a style='color:#EC8722' href='javascript:jumpPage(" + (pageBean.getPage() - 1) + ");'>上一页</a>");
-        }
-        if (pageBean.getPage() < pageBean.getPageCount()) {
-            sb.append("&nbsp;<a style='color:#EC8722' href='javascript:jumpPage(" + (pageBean.getPage() + 1) + ");'>下一页</a>");
-        }
-        sb.append("&nbsp;跳转到第");
-        sb.append("<input name='pageBean.page' class='formPageInput' onchange='changePage(this.value)' type='text' style='width:40px;' value='" + pageBean.getPage() + "' />页");
-        sb.append("<input type='button' onclick='jumpPageSub()' value=' GO ' />");
 
-        return sb.toString();
+        // 是否显示下一页
+        if (page < total) {
+            buffer.append("<li><a href=\"javascript://\" page=\"").append(page + 1).append("\">下一页</a></li>");
+        }
+
+        buffer.append("</ul></div>");
+        buffer.append("\n<input type=\"hidden\" name=\"pageBean.page\" value=\"").append(page).append("\"/>\n");
+
+        return buffer.toString();
 
     }
 }
